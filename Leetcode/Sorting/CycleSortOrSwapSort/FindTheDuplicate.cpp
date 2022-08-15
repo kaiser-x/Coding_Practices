1001c7a5b6e13208880715ca8417c4105a83b21d
kaiser-x <tk45935@gmail.com>
2022-08-15 22:38:25 +0530

    new leetcode

--- Leetcode/Sorting/CycleSortOrSwapSort/FindTheDuplicate.cpp
+++ Leetcode/Sorting/CycleSortOrSwapSort/FindTheDuplicate.cpp
@@ -18,22 +18,73 @@
 
 // Input: nums = [3,1,3,4,2]
 // Output: 3
+Cpp Code:
 
 class Solution {
 public:
+    //We make use of cyclic sort, and after sorting we check whatever the element does not
+    //match with corresponding index is the duplicate number.
+    //But the problem is the question has mentioned we cant modify given array and we ant use
+    //extra space too.
     int findDuplicate(vector<int>& nums) {
         int n=nums.size();
         for(int i=0;i<n;){
-            if(nums[i]!=nums[nums[i]-1])
-                swap(nums[i],nums[nums[i]-1]);
-            else
-                i++;
+            if(nums[i]!=nums[nums[i]-1]) swap(nums[i],nums[nums[i]-1]);
+            else i++;
         }
+        for(int i=0;i<n;i++) {
+            if(nums[i]!=i+1) return nums[i];
+        }
+        return -1;
         
-        // for(int i=0;i<n;i++) cout<<nums[i]<<" ";
+        //So we use tortois and hare method of Cycle Detection
+        int slow=nums[0],fast=nums[0];
+        while(1){
+            slow=nums[slow];
+            fast=nums[nums[fast]];
+            if(slow==fast) break;
+        }
+        slow=nums[0];
+        while(slow!=fast){
+            slow=nums[slow];
+            fast=nums[fast];
+        }
+        return slow;
+    }
+};
+
+Java Code:
+
+class Solution {
+    static void swap(int nums[],int i,int j){
+        int t=nums[i];
+        nums[i]=nums[j];
+        nums[j]=t;
+    }
+    public int findDuplicate(int[] nums) {
+        int n=nums.length;
+        for(int i=0;i<n;){
+            if(nums[i]!=nums[nums[i]-1]) swap(nums,i,nums[i]-1);
+            else i++;
+        }
         for(int i=0;i<n;i++){
             if(nums[i]!=i+1) return nums[i];
         }
-        return 0;
+        return -1;
+        
+    //As we cant modify input we have to use Cycle detect tortoise and hare method
+        
+        int slow=nums[0],fast=nums[0];
+        while(true){
+            slow=nums[slow];
+            fast=nums[nums[fast]];
+            if(slow==fast) break;
+        }
+        slow=nums[0];
+        while(slow!=fast){
+            slow=nums[slow];
+            fast=nums[fast];
+        }
+        return slow;
     }
-};
+}
